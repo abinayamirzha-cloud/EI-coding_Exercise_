d8055d71489e85467577fb8a374f48376a1cb2c2
abinayamirzha-cloud <abinayamirzha@gmail.com>
2025-10-01 18:11:52 +0530

    Update MainGame.java

--- exercise_1/creational/singleton/MainGame.java
+++ exercise_1/creational/singleton/MainGame.java
@@ -1,9 +1,19 @@
-public class MainGame {
+public class MainSingletonDemo {
     public static void main(String[] args) {
-        GameSettings settings1 = GameSettings.getInstance();
-        settings1.setVolume(80);
+        // First reference
+        GameSettings s1 = GameSettings.getInstance();
+        s1.showSettings();
 
-        GameSettings settings2 = GameSettings.getInstance();
-        System.out.println("Volume: " + settings2.getVolume()); // prints 80
+        // Change settings
+        s1.setDifficulty("Hard");
+        s1.setMaxPlayers(8);
+
+        // Second reference
+        GameSettings s2 = GameSettings.getInstance();
+        System.out.println("\nFrom second reference:");
+        s2.showSettings();
+
+        // Check both are same object
+        System.out.println("\nAre both same instance? " + (s1 == s2));
     }
 }
